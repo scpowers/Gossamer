7f27dcad6d3685143ab38e2cf80c500319ad7fa3
Spencer Powers <54383192+scpowers@users.noreply.github.com>
2020-12-27 21:52:41 -0500

    Used embedded structs to get desired behavior between regular and spiking neuron models

--- neuron.go
+++ neuron.go
@@ -1,7 +1,39 @@
 package Gossamer
 
-type Neuron struct {
+// define Neuron interface to get polymorphism-like behavior
+type Neuron interface {
+	GetSynapses() ([]int, []int)
+}
+
+// define baseline neuron struct
+type RegularNeuron struct {
 	// universal properties
 	SynIn  []int
 	SynOut []int
+}
+
+// define spiking neuron struct
+type SpikingNeuron struct {
+	RegularNeuron
+	Vmem float64 // membrane potential
+	Vt   float64 // threshold voltage
+}
+
+// regular neuron constructor
+func NewNeuron() *RegularNeuron {
+	return &RegularNeuron{}
+}
+
+// spiking neuron constructor
+func NewSpikingNeuron() *SpikingNeuron {
+	n := new(SpikingNeuron)
+	n.RegularNeuron = RegularNeuron{}
+	n.Vmem = 0
+	n.Vt = 0
+	return n
+}
+
+// get synapses of a neuron
+func (n *RegularNeuron) GetSynapses() ([]int, []int) {
+	return n.SynIn, n.SynOut
 }
